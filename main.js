7432710ab3e1b6eeb1eb6d05aafc20e4a27ad0d0
Justin Rice <jsrice7391@gmail.com>
2017-10-04 11:06:20 -0400

    WIN works

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 var guessed = [];
 var dictionary = ["Alaska", "Alabama", "Arkansas", "Arizona", "california", "Florida"];
-var word = dictionary[Math.floor(Math.random() * dictionary.length)].split("");
+var word = dictionary[Math.floor(Math.random() * dictionary.length)].toLowerCase().split("");
 var missed = 15;
-var dash = "  ____  ";
-var dashes = [];
-var indexes = [];
+var missed_guesses = [];
+
+var letter_count = 0;
+
+
 
 function show_span(letter) {
     $.each(word, function(i, l) {
         $("#blanks_list").append("<li class='inline'><p>" + l + "</p></li>");
+        guessed.push(letter);
     })
+
+    console.log(letter);
 };
 
 function show_letter(letter) {
+    guessed.push(letter);
     $.each(word, function(i, l) {
         if (letter === l) {
-            $('ul li').find('p').eq(i).show();
+            $('ul li').find('p').eq(i).addClass("show");
         }
 
     });
+
 }
 
+
+
 function get_number() {
+
     var word = dictionary[Math.floor(Math.random() * dictionary.length)].split("");
     return word
 }
@@ -40,7 +50,7 @@ $(document).ready(function() {
     show_span(word);
     get_number();
 
-    $("#guessed").html(guessed);
+    $("#guessed").html(missed_guesses);
 
     $(".inline p").hide();
 
@@ -48,13 +58,12 @@ $(document).ready(function() {
 
 
     $("#play_again").hide();
+    $("#winner").hide();
+
 
 
     $("#play_again").on("click", function() {
         get_number();
-
-
-
     })
 
 
@@ -62,27 +71,32 @@ $(document).ready(function() {
     document.onkeydown = function(letter) {
 
         var letter = letter.key;
-        guessed.push(letter);
 
         if (word.includes(letter)) {
             show_letter(letter);
 
         } else {
             console.log("No, there is no " + letter);
-            guessed.push(letter);
-            $("#guessed").html(guessed);
+            missed_guesses.push(letter);
+
             missed = missed - 1;
             $("#guesses").html(missed);
+            $("#guessed").html(missed_guesses);
         }
+        if ($('ul li').children(':visible').length == word.length) {
+            $("#winner").show();
+            // action when all are hidden
+        }
+
+
+
+
+
 
         if (missed == 0) {
             $("#guess_span").html("YOU HAVE NO MORE GUESSES");
             $("#play_again").show();
             $("guessed").hide();
-            return
-
-
-
         };
 
 
